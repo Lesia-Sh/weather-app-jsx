711957cd8a0c14e4c2d5835744536dded5d93da7
Lesia-Sh <shvch.lv@gmail.com>
2022-08-25 20:46:02 -0400

    Corrected mistakes and warnings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World!</h1>
-        <Weather city="Lviv" />
+        <Weather />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,28 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { Audio } from "react-loader-spinner";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  function showWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "0f0f10bca37612242d03bb7a3cb3baa8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(showWeather);
+    return (
+      <h2>
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="three-dots-loading"
+          wrapperStyle
+          wrapperClass
+        />
+      </h2>
     );
   }
-  let apiKey = "0f0f10bca37612242d03bb7a3cb3baa8";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <h2>
-      <Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="green"
-        ariaLabel="three-dots-loading"
-        wrapperStyle
-        wrapperClass
-      />
-    </h2>
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city here"
+          autoFocus
+          onChange={updateCity}
+        />
+        <button type="search">Search</button>
+      </form>
+    </div>
   );
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul className="weather-list">
+          <li>Temperature: {Math.round(weather.temperature)} °C</li>
+          <li>Description: {weather.description}</li>
+          <li>Humidity: {weather.humidity} %</li>
+          <li>Wind: {weather.wind} m/s</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
