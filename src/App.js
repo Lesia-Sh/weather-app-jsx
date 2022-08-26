6d0a32b599abf953409607f295cb73f3f1fc192e
Lesia-Sh <shvch.lv@gmail.com>
2022-08-26 06:48:26 -0400

    Added open source line

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <Weather />
+        <div className="Open-sourse">
+          It was coded by Lesia Shevchuk and is{" "}
+          <a
+            href="https://github.com/Lesia-Sh/weather-app-jsx"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourse
+          </a>
+        </div>
       </header>
     </div>
   );
